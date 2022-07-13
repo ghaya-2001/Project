ac8ad8f88ec9be4d4aa8c4e7bb672875864bc85b
ghaya-2001 <ghyayaderbali2001@gmail.com>
2022-07-13 14:50:36 +0100

    posts

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
-import './Posts.css'
 
+import './Posts.css'
+import React, { useEffect, useState} from 'react';
 
 const Posts =() =>{
    /* let navigate=useNavigate()*/
    /**import { useNavigate } from 'react-router-dom'; */
+
+
+
+   const [posts,setposts]=useState([{}])
+   {/*useffect*/}
+    function Posts()
+      {
+
+        fetch("https://jsonplaceholder.typicode.com/posts") /*nedina api*/
+        .then(response => response.json()) /*reponse json */
+        .then(response => setposts(response /*7atineh f west posts list*/ ))
+            
+            
+
+      
+       }
+    useEffect(() => {Posts()},[])
+
     return(
+        <div className='posts'>
+            <h1 className='titre'>POSTS</h1>
+        {posts.map((po) => (
+<div className='users'>
+<div className='user'> 
+
+     
+     
+     <div className="name">USERID : {po.userId}</div>
+     <div className='email'> TITLE : {po.title}</div>
+     <div className="id">ID : {po.id}</div>
+     <button className="button i-button">see comments</button>
+           
+  
+</div>
+</div>
 
 
-       <h1>this is the  page of posts 
-       </h1>
+
+
+        ))}
        
+       </div>
 
       
         
