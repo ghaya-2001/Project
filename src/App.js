5f1d03a803eb33589d8f11eecba08d6ceb9b4cce
ghaya-2001 <ghyayaderbali2001@gmail.com>
2022-07-15 13:05:30 +0100

    home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           
           <Route path="/Posts" element={<Posts/>}/>
-          <Route path="/Header" element={<Header/>}/>
+          <Route path="/" element={<Header/>}/>
         </Routes>
 
     </Router>
